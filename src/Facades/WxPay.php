960b975be2a8e8fcf65540cbf007f38d031693f5
alezh <watman_liu@163.com>
2017-04-25 14:05:47 +0800

    想

--- src/Facades/WxPay.php
+++ src/Facades/WxPay.php
@@ -1,7 +1,7 @@
 <?php
 namespace Alezh\Alipay\Facades;
 use Illuminate\Support\Facades\Facade;
-class Alipay extends Facade
+class WxPay extends Facade
 {
     protected static function getFacadeAccessor()
     {
--- src/config/WxPay.php
+++ src/config/WxPay.php
@@ -17,10 +17,10 @@ return[
 	 * 获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
 	 * @var string
 	 */
-    'APPID' = 'wx426b3015555a46be',
-    'MCHID' = '1900009851',
-    'KEY' = '8934e7d15453e97507ef794cf7b0519d',
-    'APPSECRET' = '7813490da6f1265e4901ffb80afaa36f',
+    'APPID' => 'wx426b3015555a46be',
+    'MCHID' => '1900009851',
+    'KEY' => '8934e7d15453e97507ef794cf7b0519d',
+    'APPSECRET' => '7813490da6f1265e4901ffb80afaa36f',
     //=======【证书路径设置】=====================================
   	/**
   	 * TODO：设置商户证书路径
@@ -28,8 +28,8 @@ return[
   	 * API证书下载地址：https://pay.weixin.qq.com/index.php/account/api_cert，下载之前需要安装商户操作证书）
   	 * @var path
   	 */
-    'SSLCERT_PATH' = '../cert/apiclient_cert.pem',
-    'SSLKEY_PATH' = '../cert/apiclient_key.pem',
+    'SSLCERT_PATH' => '../cert/apiclient_cert.pem',
+    'SSLKEY_PATH' => '../cert/apiclient_key.pem',
     //=======【curl代理设置】===================================
   	/**
   	 * TODO：这里设置代理机器，只有需要代理的时候才设置，不需要代理，请设置为0.0.0.0和0
@@ -37,8 +37,8 @@ return[
   	 * 默认CURL_PROXY_HOST=0.0.0.0和CURL_PROXY_PORT=0，此时不开启代理（如有需要才设置）
   	 * @var unknown_type
   	 */
-    'CURL_PROXY_HOST' = "0.0.0.0",
-    'CURL_PROXY_PORT' = 0,
+    'CURL_PROXY_HOST' => "0.0.0.0",
+    'CURL_PROXY_PORT' => 0,
     //=======【上报信息配置】===================================
   	/**
   	 * TODO：接口调用上报等级，默认紧错误上报（注意：上报超时间为【1s】，上报无论成败【永不抛出异常】，
@@ -47,5 +47,5 @@ return[
   	 * 上报等级，0.关闭上报; 1.仅错误出错上报; 2.全量上报
   	 * @var int
   	 */
-     'REPORT_LEVENL' = 1
+     'REPORT_LEVENL' => 1
 ];
